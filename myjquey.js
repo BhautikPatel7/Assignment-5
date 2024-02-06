809691c1f331803d5b3e58b3b9689e92470953a1
Bhautik Patel <thummarbhautik7045@gmail.com>
2024-02-06 19:13:33 +0530

    Added J Query

---
+++ myjquey.js
@@ -0,0 +1,29 @@
+
+// $(document).ready(function () {
+//     // $('P').click(function () {
+//     //     console.log("You Clicked on p");
+//     //     $(this).hide();
+//     // });
+//     // $('.hyy').click(function () {
+//     //     console.log("You Clicked On P With Class Hyy");
+//     //     $('.hyy').add('.hyy');
+//     // });
+//     // $('#it').click(function () {
+//     //     console.log("You Clicked On P With Id it");
+//     //     $('#it').hide();
+//     // });
+//     // $('P').click(function(){
+//     //     console.log("ypu clicked ", this)
+//     // });
+//     // $('#it').click();
+//     //     $('.hyy').click();
+//     // $('p').dblclick(Function(){
+//     // });
+
+
+
+// });
+$(document).ready(function(){
+
+});
+
